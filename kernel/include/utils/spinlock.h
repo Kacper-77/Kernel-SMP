99ebb6a21b5eb14a1b983eb209b68c8f19419b44
Kacper-77 <ks.kontaktowy7@gmail.com>
2026-01-15 18:28:30 +0100

    upgraded recursive spinlock

--- kernel/include/utils/spinlock.h
+++ kernel/include/utils/spinlock.h
@@ -2,6 +2,7 @@
 #define SPINLOCK_H
 
 #include <apic.h>
+#include <panic.h>
 
 extern int g_lock_enabled;
 
@@ -25,6 +26,7 @@ static inline void spin_lock(spinlock_t* target) {
         __asm__ volatile("pause");
     }
 
+    __sync_synchronize();
     target->owner = cpu_id;
     target->recursion = 1;
 }
@@ -34,12 +36,19 @@ static inline void spin_unlock(spinlock_t* target) {
 
     int cpu_id = get_current_cpu_id();
     
-    if (target->owner != cpu_id) return;
+    if (target->owner != cpu_id) {
+        panic("spin_unlock: CPU tried to unlock foreign spinlock");
+    }
+
+    if (target->recursion <= 0) {
+        panic("spin_unlock: recursion underflow");
+    }
 
     target->recursion--;
 
     if (target->recursion == 0) {
         target->owner = -1;
+        __sync_synchronize();
         __sync_lock_release(&(target->lock));
     }
 }
