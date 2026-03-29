b56bb54c50c30156fc24cba2e47f4ca80bfed8e8
Kacper-77 <ks.kontaktowy7@gmail.com>
2026-03-29 16:57:54 +0200

    fixed visibility to avoid linker noises

--- common/std_funcs.h
+++ common/std_funcs.h
@@ -4,51 +4,11 @@
 #include <stdint.h>
 #include <stddef.h>
 
-static inline int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *p1 = (const uint8_t *)s1;
-    const uint8_t *p2 = (const uint8_t *)s2;
-
-    for (size_t i = 0; i < n; i++) {
-        if (p1[i] != p2[i]) {
-            return p1[i] < p2[i] ? -1 : 1;
-        }
-    }
-    return 0;
-}
-
-static inline void* memset(void* dest, int ch, size_t count) {
-    unsigned char* ptr = (unsigned char*)dest;
-    while (count--) {
-        *ptr++ = (unsigned char)ch;
-    }
-    return dest;
-}
-
-static inline void* memcpy(void* dest, const void* src, size_t n) {
-    uint8_t* d = (uint8_t*)dest;
-    const uint8_t* s = (const uint8_t*)src;
-    while (n--) *d++ = *s++;
-    return dest;
-}
-
-static inline char* strcpy(char* dest, const char* src) {
-    char* d = dest;
-    while ((*d++ = *src++));
-    return dest;
-}
-
-static inline int strcmp(const char *s1, const char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
-}
-
-static inline size_t strlen(const char *s) {
-    size_t len = 0;
-    while (s[len]) len++;
-    return len;
-}
+int memcmp(const void *s1, const void *s2, size_t n);
+void* memset(void* dest, int ch, size_t count);
+void* memcpy(void* dest, const void* src, size_t n);
+char* strcpy(char* dest, const char* src);
+int strcmp(const char *s1, const char *s2);
+size_t strlen(const char *s);
 
 #endif
