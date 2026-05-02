7ace8b9e83c4edc98c9b5840a201a504108be782
Kacper-77 <ks.kontaktowy7@gmail.com>
2026-05-02 16:36:24 +0200

    ...

--- common/std_funcs.c
+++ common/std_funcs.c
@@ -38,3 +38,14 @@ size_t strlen(const char *s) {
     while (s[len]) len++;
     return len;
 }
+
+char* strncpy(char* dest, const char* src, size_t n) {
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    for (; i < n; i++) {
+        dest[i] = '\0';
+    }
+    return dest;
+}
--- common/std_funcs.h
+++ common/std_funcs.h
@@ -10,5 +10,6 @@ void* memcpy(void* dest, const void* src, size_t n);
 char* strcpy(char* dest, const char* src);
 int strcmp(const char *s1, const char *s2);
 size_t strlen(const char *s);
+char* strncpy(char* dest, const char* src, size_t n);
 
 #endif
